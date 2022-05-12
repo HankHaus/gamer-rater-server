e979b44d6ca4809c32ccec0f9ea30de347e028cf
Henry House <hankhaus99@gmail.com>
2022-05-12 11:15:48 -0500

    chapter 9 working

--- gamerraterapi/models/review.py
+++ gamerraterapi/models/review.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Review(models.Model):
     review_body = models.TextField()
-    gameId = models.ForeignKey("Game", on_delete=models.CASCADE)
-    gamerId = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
     
